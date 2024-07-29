daa6e6e74fde0ccd107450144882ee0e7841e63a
Alex Ripoll <alexripollpiera@gmail.com>
2024-07-29 23:00:05 +0200

    refactor builder pattern

--- src/header.rs
+++ src/header.rs
@@ -1,104 +1,87 @@
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Header {
-    id: u16,
-    query_response: bool,
-    opcode: u8,
-    authoritative_answer: bool,
-    truncated_msg: bool,
-    recursion_desired: bool,
-    recursion_available: bool,
-    reserved: u8,
-    response_code: u8,
-    question_count: u16,
-    answer_count: u16,
-    authority_count: u16,
-    additional_count: u16,
+    pub id: u16,
+    pub query_response: bool,
+    pub opcode: u8,
+    pub authoritative_answer: bool,
+    pub truncated_msg: bool,
+    pub recursion_desired: bool,
+    pub recursion_available: bool,
+    pub reserved: u8,
+    pub response_code: u8,
+    pub question_count: u16,
+    pub answer_count: u16,
+    pub authority_count: u16,
+    pub additional_count: u16,
 }
 
 impl Header {
-    pub fn id(self, id: u16) -> Self {
-        Self { id, ..self }
+    pub fn id(&mut self, id: u16) -> &mut Self {
+        self.id = id;
+        self
     }
 
-    pub fn query_response(self, query_response: bool) -> Self {
-        Self {
-            query_response,
-            ..self
-        }
+    pub fn query_response(&mut self, query_response: bool) -> &mut Self {
+        self.query_response = query_response;
+        self
     }
 
-    pub fn opcode(self, opcode: u8) -> Self {
-        Self { opcode, ..self }
+    pub fn opcode(&mut self, opcode: u8) -> &mut Self {
+        self.opcode = opcode;
+        self
     }
 
-    pub fn authoritative_answer(self, authoritative_answer: bool) -> Self {
-        Self {
-            authoritative_answer,
-            ..self
-        }
+    pub fn authoritative_answer(&mut self, authoritative_answer: bool) -> &mut Self {
+        self.authoritative_answer = authoritative_answer;
+        self
     }
 
-    pub fn truncated_msg(self, truncated_msg: bool) -> Self {
-        Self {
-            truncated_msg,
-            ..self
-        }
+    pub fn truncated_msg(&mut self, truncated_msg: bool) -> &mut Self {
+        self.truncated_msg = truncated_msg;
+        self
     }
 
-    pub fn recursion_desired(self, recursion_desired: bool) -> Self {
-        Self {
-            recursion_desired,
-            ..self
-        }
+    pub fn recursion_desired(&mut self, recursion_desired: bool) -> &mut Self {
+        self.recursion_desired = recursion_desired;
+        self
     }
 
-    pub fn recursion_available(self, recursion_available: bool) -> Self {
-        Self {
-            recursion_available,
-            ..self
-        }
+    pub fn recursion_available(&mut self, recursion_available: bool) -> &mut Self {
+        self.recursion_available = recursion_available;
+        self
     }
 
-    pub fn reserved(self, reserved: u8) -> Self {
-        Self { reserved, ..self }
+    pub fn reserved(&mut self, reserved: u8) -> &mut Self {
+        self.reserved = reserved;
+        self
     }
 
-    pub fn response_code(self, response_code: u8) -> Self {
-        Self {
-            response_code,
-            ..self
-        }
+    pub fn response_code(&mut self, response_code: u8) -> &mut Self {
+        self.response_code = response_code;
+        self
     }
 
-    pub fn question_count(self, question_count: u16) -> Self {
-        Self {
-            question_count,
-            ..self
-        }
+    pub fn question_count(&mut self, question_count: u16) -> &mut Self {
+        self.question_count = question_count;
+        self
     }
 
-    pub fn answer_count(self, answer_count: u16) -> Self {
-        Self {
-            answer_count,
-            ..self
-        }
+    pub fn answer_count(&mut self, answer_count: u16) -> &mut Self {
+        self.answer_count = answer_count;
+        self
     }
 
-    pub fn authority_count(self, authority_count: u16) -> Self {
-        Self {
-            authority_count,
-            ..self
-        }
+    pub fn authority_count(&mut self, authority_count: u16) -> &mut Self {
+        self.authority_count = authority_count;
+        self
     }
 
-    pub fn additional_count(self, additional_count: u16) -> Self {
-        Self {
-            additional_count,
-            ..self
-        }
+    pub fn additional_count(&mut self, additional_count: u16) -> &mut Self {
+        self.additional_count = additional_count;
+        self
     }
 
-    pub fn build(self) -> Header {
+    pub fn build(&self) -> Header {
         Self {
             id: self.id,
             query_response: self.query_response,
@@ -157,5 +140,44 @@ impl Header {
         bytes[11] = self.additional_count as u8;
 
         bytes
+    }
+
+    pub fn from_bytes(data: [u8; 12]) -> Header {
+        // Parse the ID
+        let id = u16::from_be_bytes([data[0], data[1]]);
+
+        // Parse the flags (2 bytes)
+        let flags = u16::from_be_bytes([data[2], data[3]]);
+
+        let query_response = (flags & 0x8000) != 0;
+        let opcode = ((flags & 0x7800) >> 11) as u8;
+        let authoritative_answer = (flags & 0x0400) != 0;
+        let truncated_msg = (flags & 0x0200) != 0;
+        let recursion_desired = (flags & 0x0100) != 0;
+        let recursion_available = (flags & 0x0080) != 0;
+        let reserved = ((flags & 0x0070) >> 4) as u8;
+        let response_code = (flags & 0x000F) as u8;
+
+        // Parse the counts
+        let question_count = u16::from_be_bytes([data[4], data[5]]);
+        let answer_count = u16::from_be_bytes([data[6], data[7]]);
+        let authority_count = u16::from_be_bytes([data[8], data[9]]);
+        let additional_count = u16::from_be_bytes([data[10], data[11]]);
+
+        Self {
+            id,
+            query_response,
+            opcode,
+            authoritative_answer,
+            truncated_msg,
+            recursion_desired,
+            recursion_available,
+            reserved,
+            response_code,
+            question_count,
+            answer_count,
+            authority_count,
+            additional_count,
+        }
     }
 }
