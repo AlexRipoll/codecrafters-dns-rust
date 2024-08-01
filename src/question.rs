c58f590ea41f985344b442622462ab1ded506e04
Alex Ripoll <alexripollpiera@gmail.com>
2024-08-01 21:10:43 +0200

    question module unit tests

--- src/question.rs
+++ src/question.rs
@@ -36,8 +36,8 @@ impl Question {
         bytes
     }
 
-    pub fn from_bytes(data: &[u8], start_idx: usize) -> Question {
-        let (label, _) = labels_from_bytes(data.clone(), start_idx);
+    pub fn from_bytes(buf: &[u8], start_pos: usize) -> Question {
+        let (label, _) = labels_from_bytes(buf, start_pos);
 
         Question::new(label, QType::A, Class::IN)
     }
@@ -52,6 +52,7 @@ fn labels_from_bytes(data: &[u8], start_pos: usize) -> (String, usize) {
         if data[cursor] & 0b11000000 == 0b11000000 {
             let offset =
                 (((data[cursor] & 0b00111111) as u16) << 8 | data[cursor + 1] as u16) as usize;
+            println!("=>> {:?}", offset);
             let (label, _) = labels_from_bytes(&data, offset);
             labels.push(label);
             // increase the index by tw0 since the offset used 2 bytes
@@ -66,4 +67,62 @@ fn labels_from_bytes(data: &[u8], start_pos: usize) -> (String, usize) {
     }
 
     (labels.join("."), cursor)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_question_to_bytes() {
+        let question = Question::new("example.com".to_string(), QType::A, Class::IN);
+        let expected_bytes = vec![
+            7, 101, 120, 97, 109, 112, 108, 101, // "example" label
+            3, 99, 111, 109, // "com" label
+            0,   // null terminator
+            0, 1, // qtype A (1)
+            0, 1, // class IN (1)
+        ];
+        assert_eq!(question.to_bytes(), expected_bytes);
+    }
+
+    #[test]
+    fn test_question_from_bytes() {
+        let bytes = vec![
+            144, 189, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, // header
+            7, 101, 120, 97, 109, 112, 108, 101, // "example" label
+            3, 99, 111, 109, // "com" label
+            0,   // null terminator
+            0, 1, // qtype A (1)
+            0, 1, // class IN (1)
+        ];
+        let question = Question::from_bytes(&bytes, 12);
+        assert_eq!(question.name, "example.com");
+        assert_eq!(question.qtype, QType::A);
+        assert_eq!(question.class, Class::IN);
+    }
+
+    #[test]
+    fn test_compressed_question_from_bytes() {
+        let bytes = vec![
+            144, 189, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, // header
+            2, 101, 110, // "en" label
+            7, 101, 120, 97, 109, 112, 108, 101, // "example" label
+            3, 99, 111, 109, // "com" label
+            0,   // null terminator
+            0, 1, // qtype A (1)
+            0, 1, // class IN (1)
+            2, 101, 115, // "es" label
+            192, 15, // pointer to "example" label
+            0,  // null terminator
+            0, 1, // qtype A (1)
+            0, 1, // class IN (1)
+        ];
+        let (label, cursor) = labels_from_bytes(&bytes, 12);
+        assert_eq!(label, "en.example.com");
+        assert_eq!(cursor, 27);
+        let (label, cursor) = labels_from_bytes(&bytes, 32);
+        assert_eq!(label, "es.example.com");
+        assert_eq!(cursor, 37);
+    }
 }
